5118ea88e9927853e60687bf2a322e9c80eb0b04
christopher-besch <mail@chris-besch.com>
2022-03-07 05:29:50 +0000

    Deploying to pages from @ christopher-besch/presentations@b5e48b49769746feb6344784f53d3cc216d581c0 🚀

--- 2022_03_07_termbaeume/berechne.java
+++ 2022_03_07_termbaeume/berechne.java
@@ -1,12 +1,12 @@
 public static int berechne(BinTree<String> root) {
     switch (root.getItem()) {
         case "+":
-            return berechne(
-                root.getLeft()) +
+            return
+                berechne(root.getLeft()) +
                 berechne(root.getRight());
         case "*":
-            return berechne(
-                root.getLeft()) *
+            return
+                berechne(root.getLeft()) *
                 berechne(root.getRight());
         default:
             return wert(root.getItem());
