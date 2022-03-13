c76a7dd881c94eaeb32241df7ac6a3122176631f
christopher-besch <mail@chris-besch.com>
2022-03-13 20:45:16 +0100

    added graphic

---
+++ 2022_03_14_neue_formeln_messunsicherheiten/graphic.py
@@ -0,0 +1,26 @@
+from manim import *
+
+
+config.transparent = True
+
+
+class Graphic(Scene):
+    def construct(self):
+        ax = Axes(
+            x_range=[0, 10, 1],
+            y_range=[0, 100, 10],
+            x_length=2,
+            x_axis_config={
+                # "numbers_to_include": np.arange(0, 10 + 0.1, 5),
+                "font_size": 24,
+            },
+        ).add_coordinates()
+        labels = ax.get_axis_labels(
+            Text(r"Winkel in °").scale(0.7),
+            Text(r"Intensität in %").scale(0.7)
+        )
+
+        dot = Dot(ax.c2p(7, 20))
+
+        self.add(ax, labels, dot)
+        
