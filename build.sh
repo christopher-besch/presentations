665d56367af176fd292fb41de2ff6d686d65cf49
christopher-besch <mail@chris-besch.com>
2025-09-02 12:01:13 +0200

    fix: build quotes

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-cd $DIR
+cd "$DIR"
 
 echo "cloning reveal.js..."
 git clone https://github.com/hakimel/reveal.js reveal || true
